0d3e8e5cc6f1f6ad86c2f1679b6b46067b7325ee
Miguel Prada <mapra99@gmail.com>
2021-05-02 03:32:20 -0500

    scale data

--- src/components/ScalingForm.jsx
+++ src/components/ScalingForm.jsx
@@ -1,7 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { Typography, TextField, Box } from '@material-ui/core'
+import { DisplacementsContext } from '../contexts/DisplacementsContext'
 
 const ScalingForm = () => {
+  const { setUnitsScale } = useContext(DisplacementsContext)
+
   const [widthMilimeters, setWidthMilimeters] = useState(null)
   const [widthPixels, setWidthPixels] = useState(null)
   const [widthScale, setWidthScale] = useState(1)
@@ -22,6 +25,12 @@ const ScalingForm = () => {
     setHeightScale(heightMilimeters / heightPixels)
   }, [heightMilimeters, heightPixels])
 
+  useEffect(() => {
+    if (!widthScale || !heightScale) return
+
+    setUnitsScale((widthScale + heightScale) * 0.5)
+  }, [widthScale, heightScale])
+
   return (
     <form>
       <Typography variant="h5">
@@ -30,15 +39,15 @@ const ScalingForm = () => {
 
       <Box m="10px 0">
         <Typography>Ancho Talud</Typography>
-        <TextField id="talud-width-mm" label="mm" onChange={e => setWidthMilimeters(e.target.value)} value={widthMilimeters} />
-        <TextField id="talud-width-px" label="px" onChange={e => setWidthPixels(e.target.value)} value={widthPixels} />
+        <TextField id="talud-width-mm" label="mm" style={{width: "50%"}} onChange={e => setWidthMilimeters(e.target.value)} value={widthMilimeters} />
+        <TextField id="talud-width-px" label="px" style={{width: "50%"}} onChange={e => setWidthPixels(e.target.value)} value={widthPixels} />
         <Typography>Relación: {widthScale}</Typography>
       </Box>
 
       <Box m="10px 0">
         <Typography>Altura Talud</Typography>
-        <TextField id="talud-height-mm" label="mm" onChange={e => setHeightMilimeters(e.target.value)} value={heightMilimeters} />
-        <TextField id="talud-height-px" label="px" onChange={e => setHeightPixels(e.target.value)} value={heightPixels} />
+        <TextField id="talud-height-mm" label="mm" style={{width: "50%"}} onChange={e => setHeightMilimeters(e.target.value)} value={heightMilimeters} />
+        <TextField id="talud-height-px" label="px" style={{width: "50%"}} onChange={e => setHeightPixels(e.target.value)} value={heightPixels} />
         <Typography>Relación: {heightScale}</Typography>
       </Box>
     </form>
--- src/contexts/DisplacementsContext.jsx
+++ src/contexts/DisplacementsContext.jsx
@@ -1,9 +1,18 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 export const DisplacementsContext = createContext(null)
 export const DisplacementsProvider = ({ children }) => {
   const [rawData, setRawData] = useState({})
+  const [scaledData, setScaledData] = useState({})
   const [loadingRawData, setLoadingRawData] = useState(false)
+  const [unitsScale, setUnitsScale] = useState(1)
+
+  const totalDisplacement = (pointData) => {
+    const { dispX, dispY } = pointData
+    return (
+      Math.sqrt((dispX || 0)**2 + (dispY || 0)**2)
+    )
+  }
 
   const loadRawData = (dispFile, direction) => {
     setLoadingRawData(true)
@@ -21,21 +30,47 @@ export const DisplacementsProvider = ({ children }) => {
           formattedData[`initial${direction}`] = parseFloat(initialValue)
           formattedData[`final${direction}`] = parseFloat(finalValue)
           formattedData[`disp${direction}`] = finalValue - initialValue
+          formattedData.totalDisp = totalDisplacement(formattedData)
 
           rawData[pointID] = formattedData
           setRawData(rawData)
         })
-
       setLoadingRawData(false)
     })
 
     reader.readAsText(dispFile)
   }
 
+  const scaleRawData = () => {
+    const transformedData = {}
+    Object.keys(rawData).forEach(pointID => {
+      const {initialX, initialY, finalX, finalY} = rawData[pointID]
+      transformedData[pointID] = {
+        initialX: initialX * unitsScale,
+        finalX: finalX * unitsScale,
+        dispX: (finalX - initialX) * unitsScale,
+        
+        initialY: initialY * unitsScale,
+        finalY: finalY * unitsScale,
+        dispY: (finalY - initialY) * unitsScale
+      }
+
+      transformedData[pointID].totalDisp = totalDisplacement(transformedData[pointID])
+    })
+    setScaledData(transformedData)
+  }
+
+  useEffect(scaleRawData, [unitsScale, rawData])
+
   const contextVal = {
     rawData,
     loadRawData,
-    loadingRawData
+    loadingRawData,
+
+    unitsScale,
+    setUnitsScale,
+
+    scaledData
   }
 
   return (
